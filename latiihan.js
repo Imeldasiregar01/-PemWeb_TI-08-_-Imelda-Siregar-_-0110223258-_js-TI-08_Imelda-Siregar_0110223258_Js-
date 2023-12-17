a6428298e6fd81b88aabc414248b7cb1f279adeb
Imeldasiregar01 <146944594+Imeldasiregar01@users.noreply.github.com>
2023-12-17 12:05:23 +0700

    Add files via upload

---
+++ latiihan.js
@@ -0,0 +1,61 @@
+// ini variabel dengan let
+let nama ="Imelda Siregar"
+console.log(nama)
+
+// ini variabel dengan const
+const firstName = "Imelda"
+const lastName = "Siregar"
+console.log(firstName + "" + lastName)
+
+// ini tipedata string
+const gender = "PR"
+console.log(typeof gender)
+
+// ini tipedata int
+const age = 19
+console.log(typeof age)
+
+// ini tipedata boolean
+const isMarried = false
+console.log(isMarried)
+console.log(typeof isMarried)
+
+
+// operator Aritmatika
+const x = 9
+const y = 10
+
+// pertambahan
+const z = x + y
+
+// pengurangan
+const m = x - y
+
+// perkalian
+const n = x * y
+
+// pembagian
+const o = x / y
+
+// pembagian
+const c = 10 % 2
+
+console.log(z)
+console.log(m)
+console.log(n)
+console.log(o)
+console.log(c)
+
+// function
+function salam() {
+    alert("Assalamualaikum")
+}
+
+salam();
+
+function isTrue() {
+    let city = "Depok"
+    console.log(city)
+}
+
+isTrue();
